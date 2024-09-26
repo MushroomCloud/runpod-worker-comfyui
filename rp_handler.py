4b6f3742df136c152d37a95514c39a3a49ddd711
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-09-26 16:54:12 +0200

    Added custom logger which supports sending logs to an API endpoint

--- rp_handler.py
+++ rp_handler.py
@@ -14,16 +14,87 @@ from requests.adapters import HTTPAdapter, Retry
 from schemas.input import INPUT_SCHEMA
 
 
+APP_NAME = 'runpod-worker-comfyui'
 BASE_URI = 'http://127.0.0.1:3000'
 VOLUME_MOUNT_PATH = '/runpod-volume'
-LOG_FILE= 'comfyui-worker.log'
+LOG_FILE = 'comfyui-worker.log'
 TIMEOUT = 600
 LOG_LEVEL = 'INFO'
 
-session = requests.Session()
-retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
-session.mount('http://', HTTPAdapter(max_retries=retries))
-rp_logger = RunPodLogger()
+
+class SnapLogHandler(logging.Handler):
+    def __init__(self, app_name: str):
+        super().__init__()
+        self.app_name = app_name
+        self.rp_logger = RunPodLogger()
+        self.rp_logger.set_level(LOG_LEVEL)
+        self.runpod_endpoint_id = os.getenv('RUNPOD_ENDPOINT_ID')
+        self.runpod_cpu_count = os.getenv('RUNPOD_CPU_COUNT')
+        self.runpod_pod_id = os.getenv('RUNPOD_POD_ID')
+        self.runpod_gpu_size = os.getenv('RUNPOD_GPU_SIZE')
+        self.runpod_mem_gb = os.getenv('RUNPOD_MEM_GB')
+        self.runpod_gpu_count = os.getenv('RUNPOD_GPU_COUNT')
+        self.runpod_volume_id = os.getenv('RUNPOD_VOLUME_ID')
+        self.runpod_pod_hostname = os.getenv('RUNPOD_POD_HOSTNAME')
+        self.runpod_debug_level = os.getenv('RUNPOD_DEBUG_LEVEL')
+        self.runpod_dc_id = os.getenv('RUNPOD_DC_ID')
+        self.runpod_gpu_name = os.getenv('RUNPOD_GPU_NAME')
+        self.log_api_endpoint = os.getenv('LOG_API_ENDPOINT')
+        self.log_api_timeout = os.getenv('LOG_API_TIMEOUT', 5)
+        self.log_api_timeout = int(self.log_api_timeout)
+        self.log_token = os.getenv('LOG_TOKEN')
+
+    def emit(self, record):
+        # Only log to RunPod logger if the length of the log entry is >= 1000 characters
+        if len(record.message) <= 1000:
+            level_mapping = {
+                logging.DEBUG: self.rp_logger.debug,
+                logging.INFO: self.rp_logger.info,
+                logging.WARNING: self.rp_logger.warn,
+                logging.ERROR: self.rp_logger.error,
+                logging.CRITICAL: self.rp_logger.error
+            }
+
+            # Wrapper to invoke RunPodLogger logging
+            rp_logger = level_mapping.get(record.levelno, self.rp_logger.info)
+            rp_logger(record.message)
+
+        log_payload = {
+            'app_name': self.app_name,
+            'log_asctime': record.asctime,
+            'log_levelname': record.levelname,
+            'log_message': record.message,
+            'runpod_endpoint_id': self.runpod_endpoint_id,
+            'runpod_cpu_count': self.runpod_cpu_count,
+            'runpod_pod_id': self.runpod_pod_id,
+            'runpod_gpu_size': self.runpod_gpu_size,
+            'runpod_mem_gb': self.runpod_mem_gb,
+            'runpod_gpu_count': self.runpod_gpu_count,
+            'runpod_volume_id': self.runpod_volume_id,
+            'runpod_pod_hostname': self.runpod_pod_hostname,
+            'runpod_debug_level': self.runpod_debug_level,
+            'runpod_dc_id': self.runpod_dc_id,
+            'runpod_gpu_name': self.runpod_gpu_name,
+            'runpod_job_id': os.getenv('RUNPOD_JOB_ID')
+        }
+
+        if self.log_api_endpoint:
+            try:
+                headers = {'Authorization': f'Bearer {self.log_token}'}
+                response = session.post(
+                    self.log_api_endpoint,
+                    json=log_payload,
+                    headers=headers,
+                    timeout=self.log_api_timeout
+                )
+                if response.status_code != 200:
+                    self.rp_logger.error(f'Failed to send log to API. Status code: {response.status_code}')
+            except requests.Timeout:
+                self.rp_logger.error(f'Timeout error sending log to API (timeout={self.log_api_timeout}s)')
+            except Exception as e:
+                self.rp_logger.error(f'Error sending log to API: {str(e)}')
+        else:
+            self.rp_logger.warn('LOG_API_ENDPOINT environment variable is not set, not logging to API')
 
 
 # ---------------------------------------------------------------------------- #
@@ -35,16 +106,16 @@ def wait_for_service(url):
 
     while True:
         try:
-            requests.get(url)
+            session.get(url)
             return
         except requests.exceptions.RequestException:
             retries += 1
 
             # Only log every 15 retries so the logs don't get spammed
             if retries % 15 == 0:
-                rp_logger.info('Service not ready yet. Retrying...')
+                logging.info('Service not ready yet. Retrying...')
         except Exception as err:
-            rp_logger.error(f'Error: {err}')
+            logging.error(f'Error: {err}')
 
         time.sleep(0.2)
 
@@ -62,6 +133,7 @@ def send_post_request(endpoint, payload):
         json=payload,
         timeout=TIMEOUT
     )
+
 
 def get_txt2img_payload(workflow, payload):
     workflow["3"]["inputs"]["seed"] = payload["seed"]
@@ -135,6 +207,7 @@ def create_unique_filename_prefix(payload):
 # ---------------------------------------------------------------------------- #
 def handler(event):
     job_id = event['id']
+    os.environ['RUNPOD_JOB_ID'] = job_id
 
     try:
         validated_input = validate(event['input'], INPUT_SCHEMA)
@@ -151,17 +224,17 @@ def handler(event):
         if workflow_name == 'default':
             workflow_name = 'txt2img'
 
-        rp_logger.info(f'Workflow: {workflow_name}', job_id)
+        logging.info(f'Workflow: {workflow_name}', job_id)
 
         if workflow_name != 'custom':
             try:
                 payload = get_workflow_payload(workflow_name, payload)
             except Exception as e:
-                rp_logger.error(f'Unable to load workflow payload for: {workflow_name}', job_id)
+                logging.error(f'Unable to load workflow payload for: {workflow_name}', job_id)
                 raise
 
         create_unique_filename_prefix(payload)
-        rp_logger.debug('Queuing prompt', job_id)
+        logging.debug('Queuing prompt', job_id)
 
         queue_response = send_post_request(
             'prompt',
@@ -173,13 +246,13 @@ def handler(event):
         if queue_response.status_code == 200:
             resp_json = queue_response.json()
             prompt_id = resp_json['prompt_id']
-            rp_logger.info(f'Prompt queued successfully: {prompt_id}', job_id)
+            logging.info(f'Prompt queued successfully: {prompt_id}', job_id)
             retries = 0
 
             while True:
                 # Only log every 15 retries so the logs don't get spammed
                 if retries == 0 or retries % 15 == 0:
-                    rp_logger.info(f'Getting status of prompt: {prompt_id}', job_id)
+                    logging.info(f'Getting status of prompt: {prompt_id}', job_id)
 
                 r = send_get_request(f'history/{prompt_id}')
                 resp_json = r.json()
@@ -197,7 +270,7 @@ def handler(event):
                 outputs = resp_json[prompt_id]['outputs']
 
                 if len(outputs):
-                    rp_logger.info(f'Images generated successfully for prompt: {prompt_id}', job_id)
+                    logging.info(f'Images generated successfully for prompt: {prompt_id}', job_id)
                     image_filenames = get_filenames(outputs)
                     images = []
 
@@ -208,7 +281,7 @@ def handler(event):
                         with open(image_path, 'rb') as image_file:
                             images.append(base64.b64encode(image_file.read()).decode('utf-8'))
 
-                        rp_logger.info(f'Deleting output file: {image_path}', job_id)
+                        logging.info(f'Deleting output file: {image_path}', job_id)
                         os.remove(image_path)
 
                     return {
@@ -240,15 +313,15 @@ def handler(event):
             except Exception as e:
                 queue_response_content = str(queue_response.content)
 
-            rp_logger.error(f'HTTP Status code: {queue_response.status_code}', job_id)
-            rp_logger.error(queue_response_content, job_id)
+            logging.error(f'HTTP Status code: {queue_response.status_code}', job_id)
+            logging.error(queue_response_content, job_id)
 
             return {
                 'error': f'HTTP status code: {queue_response.status_code}',
                 'output': queue_response_content
             }
     except Exception as e:
-        rp_logger.error(f'An exception was raised: {e}', job_id)
+        logging.error(f'An exception was raised: {e}', job_id)
 
         return {
             'error': traceback.format_exc(),
@@ -256,20 +329,21 @@ def handler(event):
         }
 
 
-if __name__ == '__main__':
-    # Setup log file
-    logging.getLogger().setLevel(LOG_LEVEL)
-    log_handler = logging.handlers.WatchedFileHandler(f'{VOLUME_MOUNT_PATH}/{LOG_FILE}')
+def setup_logging():
     formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s')
+    log_handler = SnapLogHandler(APP_NAME)
     log_handler.setFormatter(formatter)
     logging.getLogger().addHandler(log_handler)
 
-    # Set up RunPod logger
-    rp_logger.set_level(LOG_LEVEL)
 
+if __name__ == '__main__':
+    session = requests.Session()
+    retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
+    session.mount('http://', HTTPAdapter(max_retries=retries))
+    setup_logging()
     wait_for_service(url=f'{BASE_URI}/system_stats')
-    rp_logger.info('ComfyUI API is ready')
-    rp_logger.info('Starting RunPod Serverless...')
+    logging.info('ComfyUI API is ready')
+    logging.info('Starting RunPod Serverless...')
     runpod.serverless.start(
         {
             'handler': handler
